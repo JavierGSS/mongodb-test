52a362cb605f809712f8584a5c09828c6459bb5d
Baba <jagasal@icloud.com>
2024-05-01 19:39:19 -0600

    edit pipeline2

--- app2.js
+++ app2.js
@@ -18,13 +18,28 @@ const pipeline = [
   },
 ];
 
+const pipeline2 = [
+  { $match: { amount: { $gte: 10 }, account_type: "Credit" } },
+  {
+    $sort: { amount: -1 },
+  },
+  {
+    $project: {
+      _id: 0,
+      amount: 1,
+      account_type: 1,
+      mxn_amount: { $divide: ["$amount", 0.058] },
+    },
+  },
+];
+
 const main = async () => {
   try {
     // We establish a connection to MongoDB and run our aggregation pipeline
     await client.connect();
     console.log(`DB connection success. \nFull connection string: ${uri}`);
     let comments = client.db("test").collection("comments");
-    let result = comments.aggregate(pipeline);
+    let result = comments.aggregate(pipeline2);
     for await (let doc of result) {
       console.log(doc);
     }
